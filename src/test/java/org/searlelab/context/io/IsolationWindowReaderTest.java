23a97064756c61d52f31024d72e485d84c39a615
Ariana Shannon <ashannon1229@gmail.com>
2026-08-04 07:21:09 -0500

    Fixed the URL to find the resource for the IsolationWindowReaderTest in test resources.

--- src/test/java/org/searlelab/context/io/IsolationWindowReaderTest.java
+++ src/test/java/org/searlelab/context/io/IsolationWindowReaderTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Objects;
+import org.checkerframework.common.reflection.qual.GetClass;
 import org.junit.Test;
 
 import org.searlelab.context.io.IsolationWindowReader;
@@ -14,7 +16,12 @@ public class IsolationWindowReaderTest {
 
 	@Test
 	public void testThatFileWorks() throws Throwable {
-		URL massListFile = getClass().getClassLoader().getResource("IL2A_GPFDIA_0combined_masked0_assay.txt");
+
+		// Does this correctly read a mass list? Read the mass list, and determine if
+		// the first two precursor values and retention times are the same.
+		// For the file used in the test case, this is true because its a target-decoy
+		// pair/
+		URL massListFile = Objects.requireNonNull(getClass().getResource("/org/searlelab/context/io/IL2A_GPFDIA_0combined_masked0_assay.txt"));
 
 		String massListPath = Paths.get(massListFile.toURI()).toString();
 		System.out.println("Mass list test file is " + massListPath);
