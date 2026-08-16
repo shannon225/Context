fbc20afe37c7a8bd104b26647d6017b7a73d41f1
Ariana Shannon <ashannon1229@gmail.com>
2026-08-16 15:46:14 -0500

    Small changes to ContextMProphet to clean up the code.

--- src/main/java/org/searlelab/context/mprophet/ContextMProphetExecutorCLI.java
+++ src/main/java/org/searlelab/context/mprophet/ContextMProphetExecutorCLI.java
@@ -71,13 +71,15 @@ public class ContextMProphetExecutorCLI {
 				validateReadableFile("DIA", diaFilePath);
 		
 				ContextMProphetExecutor.executeMProphet(libraryPath, fastaPath, diaFilePath, massListPath);
+				
 			} else if (MODE_MPROPHET_FOLDER.equals(mode)) {
 				
 				String diaFolderPath = requireArguments(arguments, "--dia-folder");
 				File diaFolder = validateDirectory("DIA folder", diaFolderPath);
+
+				ContextMProphetExecutor.executeMProphetOnFolder(libraryPath, fastaPath, diaFolder);
 				
-				ContextMProphetExecutor.executeMProphetOnFolder(libraryPath,  fastaPath, diaFolder);
-							} else {
+			} else {
 				throw new IllegalArgumentException(
 						"Unknown mode '" + mode + "'. Expected context, context-folder, mprophet or mprophet-folder.");
 			}
