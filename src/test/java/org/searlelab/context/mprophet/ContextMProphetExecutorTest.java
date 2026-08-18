shannon225/contextguide

Began ContextMProphetExecutorTest and added a check for the expected output in ContextFeatureScorerTest.

src/test/java/org/searlelab/contextguide/mprophet/ContextMProphetExecutorTest.java
@@ -1,0 +1,67 @@
+package org.searlelab.contextguide.mprophet;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class ContextMProphetExecutorTest {
+
+	@Rule 
+	public TemporaryFolder tempFolder = TemporaryFolder.builder().assureDeletion().build();
+	
+	@Test
+	public void test() throws Throwable {
+
+		// Map files 
+		URL libraryFileName = getClass().getClassLoader().getResource("IL2_and_IL15_Combo.elib");
+		URL fastaFileName = getClass().getClassLoader().getResource("mus_musculus_reviewed_uniprot.fasta");
+
+		// Where the feature files are located: 
+		URL diaFileName = getClass().getClassLoader().getResource("IL2A_GPFDIA_0combined_masked0_assay.dia");
+
+		// Are all the paths mapped to real files? 
+		assertNotNull("Library was not found. ", libraryFileName);
+		assertNotNull("Fasta was not found. ", fastaFileName);
+		assertNotNull("DIA file not found. ", diaFileName);
+
+		Path libraryPath = Paths.get(libraryFileName.toURI());
+		Path fastaPath = Paths.get(fastaFileName.toURI());
+		Path diaPath = Paths.get(diaFileName.toURI());
+
+		File diaFile = diaPath.toFile();
+		//		File[] diaFiles = diaFolder.listFiles();
+
+		//		System.out.println("DIA Folder was identified as: " + diaFolder.getAbsolutePath());
+		//		System.out.println("DIA files detected! The following files will be processed with MProphet:" + diaFolder.listFiles());
+
+
+		// Ignore files that do not end in .dia 
+		if (!diaFile.isFile() && !diaFile.getName().endsWith(".dia")) {
+			
+		
+
+		String diaName = diaFile.getName(); 
+		String baseName = diaName.substring(0, diaName.lastIndexOf(".dia"));
+
+		File massListFile = new File(diaFile, baseName + ".txt");
+		String massListPath = massListFile.getAbsolutePath();
+
+		System.out.println("Processessing " + diaFile.getName());
+
+		if (!massListFile.exists()) {
+			System.out.println("Skipping " + diaFile.getName() + " because mass list was not found: " + massListPath);
+			
+		}
+		ContextMProphetExecutor.executeContextMProphet(libraryPath.toString(), fastaPath.toString(), diaPath.toString(), massListPath);
+	}	
+}
+	
+}
+

shannon225/Context

src/test/java/org/searlelab/context/mprophet/ContextMProphetExecutorTest.java
@@ -1,0 +1,67 @@
+package org.searlelab.context.mprophet;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class ContextMProphetExecutorTest {
+
+	@Rule 
+	public TemporaryFolder tempFolder = TemporaryFolder.builder().assureDeletion().build();
+	
+	@Test
+	public void test() throws Throwable {
+
+		// Map files 
+		URL libraryFileName = getClass().getClassLoader().getResource("IL2_and_IL15_Combo.elib");
+		URL fastaFileName = getClass().getClassLoader().getResource("mus_musculus_reviewed_uniprot.fasta");
+
+		// Where the feature files are located: 
+		URL diaFileName = getClass().getClassLoader().getResource("IL2A_GPFDIA_0combined_masked0_assay.dia");
+
+		// Are all the paths mapped to real files? 
+		assertNotNull("Library was not found. ", libraryFileName);
+		assertNotNull("Fasta was not found. ", fastaFileName);
+		assertNotNull("DIA file not found. ", diaFileName);
+
+		Path libraryPath = Paths.get(libraryFileName.toURI());
+		Path fastaPath = Paths.get(fastaFileName.toURI());
+		Path diaPath = Paths.get(diaFileName.toURI());
+
+		File diaFile = diaPath.toFile();
+		//		File[] diaFiles = diaFolder.listFiles();
+
+		//		System.out.println("DIA Folder was identified as: " + diaFolder.getAbsolutePath());
+		//		System.out.println("DIA files detected! The following files will be processed with MProphet:" + diaFolder.listFiles());
+
+
+		// Ignore files that do not end in .dia 
+		if (!diaFile.isFile() && !diaFile.getName().endsWith(".dia")) {
+			
+		
+
+		String diaName = diaFile.getName(); 
+		String baseName = diaName.substring(0, diaName.lastIndexOf(".dia"));
+
+		File massListFile = new File(diaFile, baseName + ".txt");
+		String massListPath = massListFile.getAbsolutePath();
+
+		System.out.println("Processessing " + diaFile.getName());
+
+		if (!massListFile.exists()) {
+			System.out.println("Skipping " + diaFile.getName() + " because mass list was not found: " + massListPath);
+			
+		}
+		ContextMProphetExecutor.executeContextMProphet(libraryPath.toString(), fastaPath.toString(), diaPath.toString(), massListPath);
+	}	
+}
+	
+}
+